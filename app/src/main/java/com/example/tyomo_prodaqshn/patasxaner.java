2300e6bf812ed27441394a8976460a5ed958de4a
Artyom-Bdat <artarsargsyaner@gmail.com>
2024-05-26 18:45:48 +0400

    100%

--- app/src/main/java/com/example/tyomo_prodaqshn/patasxaner.java
+++ app/src/main/java/com/example/tyomo_prodaqshn/patasxaner.java
@@ -4,7 +4,7 @@ public class patasxaner {                                                       
     public static String []  patasxan = new String[]{"20","81","50","30","25","20","123","27","47","80","30","18","23","10","0.5","25","27"};
     public static String []  patasxan_2 = new String[]{"60","28","14","9","49","21"};
     public static String []  modek_3d = new String[]{"54","110","140","68","220","290","100"};
-    public static String []  Hesht = new String[]{"22.30","9.18","18","50","27.25","16.10"};
+    public static String []  Hesht = new String[]{"22.30","9.18","18","50","17.25","16.10"};
     public static String []  madium = new String[]{"247","12","55.25","45","24","125"};
-    public static String []  hard = new String[]{"39","60","56","161","56","240"};
+    public static String []  hard = new String[]{"39","60","196","161","56","120"};
 }
